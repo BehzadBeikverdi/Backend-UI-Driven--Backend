575fdee39afc5afd0d9353981d0d1e9e9a0c97e2
BehzadBeikverdi <behzadevstack@gmail.com>
2026-02-10 12:36:48 +0330

    First Commit

---
+++ src/main/java/ir/pishgaman/backend_data_driven_demo/BackendDataDrivenDemoApplication.java
@@ -0,0 +1,13 @@
+package ir.pishgaman.backend_data_driven_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDataDrivenDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendDataDrivenDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/pishgaman/backend_data_driven_demo/BackendDataDrivenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.pishgaman.backend_data_driven_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDataDrivenDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
